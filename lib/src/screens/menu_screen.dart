ee7196d89edd0e70c52739a9962be005ccc616be
BOM <mtuan.0111@gmail.com>
2026-06-28 23:06:31 +0700

    refactor: remove unused package_info_plus dependency from menu_screen

--- lib/src/screens/menu_screen.dart
+++ lib/src/screens/menu_screen.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:package_info_plus/package_info_plus.dart';
 import 'package:skeleton_core/skeleton_core.dart';
 
 /// A generic, reusable Main Menu screen for all game apps.
